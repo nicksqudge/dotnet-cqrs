a8ad25ef7ffe2c1843a1f9bc4079766f035c6ff9
Nick Browne <nick@squdge.me>
2022-04-02 12:35:13 +0100

    Added code snippets

---
+++ examples/ApiExample/ApplicationLayer/Queries/ViewCart/ViewCartQuery.cs
@@ -0,0 +1,24 @@
+﻿using DotnetCQRS;
+using DotnetCQRS.Queries;
+
+namespace ApiExample.ApplicationLayer.Queries.ViewCart
+{
+
+
+    public class ViewCartQuery : IQuery<ViewCartResult>
+    {
+    }
+
+    public class ViewCartResult
+    {
+
+    }
+
+    public class ViewCartHandler : IQueryHandler<ViewCartQuery, ViewCartResult>
+    {
+        public Task<Result<ViewCartResult>> HandleAsync(ViewCartQuery query, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
